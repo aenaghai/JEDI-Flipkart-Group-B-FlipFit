e1e860f63c19e3c841afe59bcc3738a2b4ad09c1
aena.ghai <aenaghai5@gmail.com>
2024-06-26 12:04:26 +0530

    Added customer interface and gymOwner interface

---
+++ JEDI-Flipit-POS-Java/src/main/java/com.flipkart/business/CustomerInterface.java
@@ -0,0 +1,19 @@
+package com.flipkart.business;
+
+import com.flipkart.bean.Booking;
+import com.flipkart.bean.FlipFitGym;
+import com.flipkart.bean.Slot;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public interface CustomerInterface {
+    public List<FlipFitGym> viewAllGymCenters(String city);
+    public List<Slot> viewAllFreeSlots (String gymId, LocalDate date);
+    public List<Booking> viewAllBookings (String gymId, LocalDate date);
+    public boolean bookSlot (String userId, String gymId, String slotId, LocalDate date, LocalTime time);
+    public boolean cancelSlot (String gymId, LocalDate date, LocalTime time);
+    public boolean checkValidCustomer (String userName, String password);
+    public boolean registerCustomer(String userName, String password, String email, String city, String cardNumber);
+}
